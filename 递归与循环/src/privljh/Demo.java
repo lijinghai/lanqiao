7e567f705d744c696486bb6dfdfb0f0279dc171a
Dorian1015 <lijinghailjh@163.com>
2021-04-12 23:59:29 +0800

    递归与循环

--- 递归与循环/src/privljh/Demo.java
+++ 递归与循环/src/privljh/Demo.java
@@ -2,6 +2,6 @@ package privljh;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello world1");
+        System.out.println("Hello world");
     }
 }
